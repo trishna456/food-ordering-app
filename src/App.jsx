2ca268bf0e3848f4830b23bdf98cf0d47b629493
Trishna <trishna.patil13@gmail.com>
2024-07-04 17:54:14 -0400

    added header, meals component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Header from "./components/Header";
+import Meals from "./components/Meals";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Meals />
+    </>
+  );
 }
 
 export default App;
