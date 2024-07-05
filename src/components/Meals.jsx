dfe71cadbfd7605cb22e26afb6252ed7c06a7175
Trishna <trishna.patil13@gmail.com>
2024-07-05 16:06:05 -0400

    created custom hook for sending http requests

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,25 +1,20 @@
 import { useEffect, useState } from 'react';
 import MealItem from './MealItem';
+import useHttp from '../hooks/useHttp';
 
+const requestConfig = {};
+// creating config obj outside the component function to avoid infinite loops
+// Since config is added as a dependency in useHttp
 export default function Meals() {
-  const [loadedMeals, setLoadedMeals] = useState([]);
+  const {
+    data: loadedMeals,
+    isLoading,
+    error,
+  } = useHttp('http://localhost:3000/meals', requestConfig, []);
 
-  useEffect(() => {
-    async function fetchMeals() {
-      try {
-        const response = await fetch('http://localhost:3000/meals');
-
-        if (!response.ok) {
-          //...
-        }
-
-        const meals = await response.json();
-        setLoadedMeals(meals);
-      } catch (err) {}
-    }
-
-    fetchMeals();
-  }, []);
+  if (isLoading) {
+    return <p>Fetching meals...</p>;
+  }
 
   return (
     <ul id='meals'>
---
+++ src/hooks/useHttp.js
@@ -0,0 +1,62 @@
+import { useCallback, useEffect, useState } from 'react';
+
+// helper utility function for general dealing with sending request
+async function sendHttpRequest(url, config) {
+  const response = await fetch(url, config);
+
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error(
+      resData.message || 'Something went wrong, failed to send request.'
+    );
+  }
+
+  return resData;
+}
+
+export default function useHttp(url, config, initialData) {
+  const [data, setData] = useState(initialData);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  /*
+   * Wrapping sendRequest with useCallback
+   * to prevent creation of a new function object everytime the state changes and the component is re-rendered
+   * since we are updating some state in this function, it will cause the component that uses this hook to execute again
+   * this will cause the effect function to run again
+   * because sendRequest will change everytime the surrounding component executes again
+   * because a new function object will be created
+   * so wrapping it with a useCallback
+   */
+
+  const sendRequest = useCallback(
+    // send request and updating state based on request status
+    async function sendRequest() {
+      setIsLoading(true);
+      try {
+        const resData = await sendHttpRequest(url, config);
+        setData(resData);
+      } catch (error) {
+        setError(error.message || 'Something went wrong!');
+      }
+      setIsLoading(false);
+    },
+    [url, config]
+  );
+
+  useEffect(() => {
+    // in this app, get request must be sent when the component to which it belongs gets rendered
+    if ((config && (config.method === 'GET' || !config.method)) || !config) {
+      sendRequest();
+    }
+  }, [sendRequest, config]); // since sendRequest is defined outside useEffect, adding it as a dependency here
+
+  return {
+    // exposing the states and function to all components that will use this hook
+    data,
+    isLoading,
+    error,
+    sendRequest,
+  };
+}
